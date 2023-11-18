1aaeae48b243fd1135eb2f631556b177168ede0b
Scott McCraw <scott@fiero.live>
2023-11-17 21:11:19 -0800

    adding github link

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,11 +8,16 @@ export function HomePage() {
             this space and as I encounter new and interesting techniques I will continue adding my discoveries here.
         </p>
         <p>If you have any comments, feedback or especially any pointers to new and interesting techniques, please
-            drop me a line at <a href='mailto: smccraw116@gmail.com'>smccraw116@gmail.com</a>.
+            drop me a line at <a href='mailto: smccraw116@gmail.com'>smccraw116@gmail.com</a>. If you want to take
+            a look at the code (React app + P5.js), the github project is <a href='https://github.com/smccraw/generative-art'>https://github.com/smccraw/generative-art</a>.
         </p>
         <p>Puruse the sections below or if you want to just jump in and look at the finished artwork, you can
             select the <a href='./finished'>Finished</a> tab above.
         </p>
+        <p>Also note that due to the amount of large images and large image generating canvases used here,
+            this site is not very mobile friendly. Best viewed on a laptop of full PC.
+        </p>
+        <br />
         <h2>Lines, lines and more lines</h2>
         <p>The most obvious place for me to start was my memories of the old Spirograph toy I had as a kid. using
             just circles and ovals as templates you could generate seemingly endless patterns very easily. Computers
@@ -20,14 +25,14 @@ export function HomePage() {
             any of the below thumbs if you want to watch the animations of these images being generated.
         </p>
         <div className="thumbnail-set">
-            <a href='./lineart#simpletrig'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/trigFlower_thumb.jpg'></img></a>
-            <a href='./lineart#lineflower'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/trigFlower2_thumb.jpg'></img></a>
-            <a href='./lineart#linetorus'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/lineTorus_thumb.jpg'></img></a>
-            <a href='./lineart#linearches'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/lineArches_thumb.jpg'></img></a>
-            <a href='./lineart#mechbloom1'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/mechanicalBloom1_thumb.jpg'></img></a>
-            <a href='./lineart#mechbloom2'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/mechanicalBloom2_thumb.jpg'></img></a>
-            <a href='./lineart#straightcircle'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/lineCircle_thumb.jpg'></img></a>
-            <a href='./lineart#straightcircle2'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/lineCircle2_thumb.jpg'></img></a>
+            <a href='./lineart#simpletrig'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/trigFlower_thumb.jpg'></img></a>
+            <a href='./lineart#lineflower'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/trigFlower2_thumb.jpg'></img></a>
+            <a href='./lineart#linetorus'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/lineTorus_thumb.jpg'></img></a>
+            <a href='./lineart#linearches'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/lineArches_thumb.jpg'></img></a>
+            <a href='./lineart#mechbloom1'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/mechanicalBloom1_thumb.jpg'></img></a>
+            <a href='./lineart#mechbloom2'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/mechanicalBloom2_thumb.jpg'></img></a>
+            <a href='./lineart#straightcircle'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/lineCircle_thumb.jpg'></img></a>
+            <a href='./lineart#straightcircle2'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/lineCircle2_thumb.jpg'></img></a>
         </div>
         
         <h2>Wiggly Lines</h2>
@@ -35,46 +40,46 @@ export function HomePage() {
             at the small scale, can also come together to make some very interesting things.
         </p>
          <div className="thumbnail-set">
-            <a href='./wiggles#wigglysplines'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/wigglyLine.jpg'></img></a>
-            <a href='./wiggles#wigglysplinecircle'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/wigglySpline_thumb.jpg'></img></a>
-            <a href='./wiggles#wigglyropes'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/wigglyRope4.jpg'></img></a>
+            <a href='./wiggles#wigglysplines'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/wigglyLine.jpg'></img></a>
+            <a href='./wiggles#wigglysplinecircle'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/wigglySpline_thumb.jpg'></img></a>
+            <a href='./wiggles#wigglyropes'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/wigglyRope4.jpg'></img></a>
           
         </div>
 
         <h2>Parametric Equations</h2>
         <p>Using mathematical algorithms to generate smooth shapes and curves,combined with different painting techniques.</p>
          <div className="thumbnail-set">
-            <a href='./parametrics#parabutterfly'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterflyCurve1_thumb.jpg'></img></a>
-            <a href='./parametrics#sandybutterfly'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterflyCurve2_thumb.jpg'></img></a>
-            <a href='./parametrics#smokeybutterfly'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterflyCurve3_thumb.jpg'></img></a>
-            <a href='./parametrics#parasplat'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterfly3D_thumb.jpg'></img></a>
-            <a href='./parametrics#fierybutterfly'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterFireFly_thumb.jpg'></img></a>
-            <a href='./parametrics#paraspirals'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/paraSpirals_thumb.jpg'></img></a>
+            <a href='./parametrics#parabutterfly'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterflyCurve1_thumb.jpg'></img></a>
+            <a href='./parametrics#sandybutterfly'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterflyCurve2_thumb.jpg'></img></a>
+            <a href='./parametrics#smokeybutterfly'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterflyCurve3_thumb.jpg'></img></a>
+            <a href='./parametrics#parasplat'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterfly3D_thumb.jpg'></img></a>
+            <a href='./parametrics#fierybutterfly'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/butterFireFly_thumb.jpg'></img></a>
+            <a href='./parametrics#paraspirals'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/paraSpirals_thumb.jpg'></img></a>
         </div>
 
         <h2>Attractors</h2>
         <p>Strange or otherwise, these dynamic systems have a certain beauty to them.</p>
          <div className="thumbnail-set">
-            <a href='./attractors'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/clifford1_thumb.jpg'></img></a>
-            <a href='./attractors'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/clifford2_thumb.jpg'></img></a>
-            <a href='./attractors'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/clifford3_thumb.jpg'></img></a>
-            <a href='./attractors'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/dejong1_thumb.jpg'></img></a>
-            <a href='./attractors'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/dejong2_thumb.jpg'></img></a>
-            <a href='./attractors'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/hopalong_thumb.jpg'></img></a>
-            <a href='./attractors'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/gumowski_thumb.jpg'></img></a>
+            <a href='./attractors'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/clifford1_thumb.jpg'></img></a>
+            <a href='./attractors'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/clifford2_thumb.jpg'></img></a>
+            <a href='./attractors'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/clifford3_thumb.jpg'></img></a>
+            <a href='./attractors'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/dejong1_thumb.jpg'></img></a>
+            <a href='./attractors'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/dejong2_thumb.jpg'></img></a>
+            <a href='./attractors'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/hopalong_thumb.jpg'></img></a>
+            <a href='./attractors'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/gumowski_thumb.jpg'></img></a>
         </div>
 
         <h2>Harmonographs - the Pendulum(s) Swing Both Ways</h2>
         <p>Dating back to the mid-19th century, drawing pictures with a swinging pendulum has fascinated mathematicians and inventors alike.</p>
          <div className="thumbnail-set">
-            <a href='./harmonographs'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph_thumb.jpg'></img></a>
-            <a href='./harmonographs'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph_BW_thumb.jpg'></img></a>
-            <a href='./harmonographs'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph1_thumb.jpg'></img></a>
-            <a href='./harmonographs'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph2_thumb.jpg'></img></a>
-            <a href='./harmonographs'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph3_thumb.jpg'></img></a>
-            <a href='./harmonographs'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph4_thumb.jpg'></img></a>
-            <a href='./harmonographs'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph5_thumb.jpg'></img></a>
-            <a href='./harmonographs'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph6_thumb.jpg'></img></a>
+            <a href='./harmonographs'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph_thumb.jpg'></img></a>
+            <a href='./harmonographs'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph_BW_thumb.jpg'></img></a>
+            <a href='./harmonographs'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph1_thumb.jpg'></img></a>
+            <a href='./harmonographs'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph2_thumb.jpg'></img></a>
+            <a href='./harmonographs'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph3_thumb.jpg'></img></a>
+            <a href='./harmonographs'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph4_thumb.jpg'></img></a>
+            <a href='./harmonographs'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph5_thumb.jpg'></img></a>
+            <a href='./harmonographs'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/harmonograph6_thumb.jpg'></img></a>
         </div>
 
         <h2>Autonomous Bots</h2>
@@ -82,9 +87,9 @@ export function HomePage() {
             based on it's current internal state and its limited observations of the world around it.
         </p>
          <div className="thumbnail-set">
-            <a href='./autobots'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/friendFollow1_thumb.jpg'></img></a>
-            <a href='./autobots#flourishb'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/flourishB_thumb.jpg'></img></a>
-            <a href='./autobots#flourishheron'><img alt='thumbnail'>className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/flourishHeron_thumb.jpg'></img></a>
+            <a href='./autobots'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/friendFollow1_thumb.jpg'></img></a>
+            <a href='./autobots#flourishb'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/flourishB_thumb.jpg'></img></a>
+            <a href='./autobots#flourishheron'><img alt='thumbnail' className='thumbnail' src='https://gen-art-images.s3.us-west-2.amazonaws.com/flourishHeron_thumb.jpg'></img></a>
         </div>
     </div>
 }
